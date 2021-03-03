66677972558959376460f41200f50a9d6d029b9d
sukhmani <sukhmani@bellevuecollege.edu>
2021-03-02 18:19:09 -0800

    scene1

---
+++ js/Scene1.js
@@ -0,0 +1,26 @@
+class GamePlay extends Phaser.Scene {
+
+    constructor() {
+        super({ key: "GamePlay" });
+
+    }
+    preload(){
+        this.load.image("sugerField", "assets/sugerFields.jpg");
+        this.load.image("grass", "assets/grass.jpg");
+
+    }
+
+    
+
+    create(){
+        this.add.image(50,50,"sugerField");
+        platforms = this.physics.add.staticGroup();
+        //this.add.image(50,50,"grass");
+        platforms.create(400, 568, 'grass').setScale(2).refreshBody();
+    }
+    update(){
+
+    }
+
+
+}; 
